529c4a43435c5548045ec1acd8937bf0327ec652
Vahn <vahnmarty@gmail.com>
2022-11-14 01:48:31 +0800

    ui login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,57 +1,71 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-input-label for="email" :value="__('Email')" />
-
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-green-700">{{ __("Welcome back") }}</h2>
+          <p class="mt-2 text-center text-sm">{{ __("Let's build something great") }}</p>
+        </div>
+      
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form class="space-y-6" action="{{ route('login') }}" method="POST">
+                @csrf
+    
+                <x-frontend-form.form-group>
+                    <x-slot:label>
+                        <x-input-label for="email" value="{{ __('Email Address') }}"/>
+                    </x-slot:label>
+                    <x-frontend-form.input-text id="email" name="email" required/>
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </x-frontend-form.form-group>
+    
+                <x-frontend-form.form-group>
+                    <x-slot:label>
+                        <x-input-label for="password" value="{{ __('Password') }}"/>
+                    </x-slot:label>
+                    <x-frontend-form.input-text id="password" name="password" required/>
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </x-frontend-form.form-group>
+    
+      
+              <x-frontend-form.button-primary>{{ __('Register') }}</x-frontend-form.button-primary>
+            </form>
+      
+            <div class="mt-6">
+              <div class="relative">
+                <div class="absolute inset-0 flex items-center">
+                  <div class="w-full border-t border-gray-300"></div>
+                </div>
+                <div class="relative flex justify-center text-sm">
+                  <span class="bg-white px-2 text-gray-500">{{ __('Or do it via other accounts') }}</span>
+                </div>
+              </div>
+      
+              
+              <section class="mt-8 flex justify-center gap-4 item-center">
+                  <a href="#">
+                      <img src="{{ asset('img/icons/google.svg') }}" alt="" class="w-8 h-8">
+                  </a>
+                  <a href="#">
+                    <img src="{{ asset('img/icons/apple.svg') }}" alt="" class="w-8 h-8">
+                </a>
+                <a href="#">
+                    <img src="{{ asset('img/icons/facebook.svg') }}" alt="" class="w-8 h-8">
+                </a>
+              </section>
+    
+              <section class="mt-8">
+                  <p class="text-center">
+                      <span>{{ __("Forgot password? ") }}</span>
+                      <a href="{{ route('password.request') }}" class="text-green-700 font-bold">{{ __("Reset Password") }}</a>
+                  </p>
+              </section>
+    
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+          </div>
+        </div>
+      </div>
+    
+      
+    </x-guest-layout>
+    
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,8 +8,8 @@
   
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-        <form class="space-y-6" action="#" method="POST">
-
+        <form class="space-y-6" action="{{ route('register') }}" method="POST">
+            @csrf
             <x-frontend-form.form-group>
                 <x-slot:label>
                     <x-input-label for="name" value="{{ __('Your Name') }}"/>
