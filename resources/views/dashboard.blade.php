46bef0c3671569cb78b47f0691bc5288f34a903a
Vahn <vahnmarty@gmail.com>
2022-12-18 15:17:45 +0800

    dashboard ui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,24 +1,14 @@
 <x-app-layout>
+
+    @section('title', 'Dashboard')
+    
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <x-slot name="rightHeader">
-        <div class="grid grid-cols-2 gap-4">
-            <form id="form-clinic">
-                <x-form.select name="clinic_id" onchange="document.querySelector('#form-clinic').submit()">
-                    <option value="">-- {{ __('All Clinic ') }} --</option>
-                    @foreach($clinics as $clinic)
-                    <option value="{{ $clinic->id }}">{{ $clinic->name }}</option>
-                    @endforeach
-                </x-form.select>
-            </form>
-        </div>
-    </x-slot>
-
-    <div class="py-12">
+    <div class="py-6">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 
             <section>
--- resources/views/livewire/patient/dashboard-patients.blade.php
+++ resources/views/livewire/patient/dashboard-patients.blade.php
@@ -1,9 +1,6 @@
 <div>
-    <header class="flex justify-between">
-        <h3 class="text-2xl font-bold">{{ __('Patient List') }}</h3>
-    </header>
 
-    <div class="mt-8">
+    <div>
         {{ $this->table }}
     </div>
 </div>
